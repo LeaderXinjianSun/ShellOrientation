32411ce1ab6eefcb1c864e71f18ed9ef5e5970d2
sunxinjian <715054307@qq.com>
2022-12-26 08:42:32 +0800

    添加软件打开与关闭逻辑

--- ShellOrientation/App.xaml.cs
+++ ShellOrientation/App.xaml.cs
@@ -5,6 +5,8 @@ using ShellOrientation.ViewModels.Home;
 using ShellOrientation.Views;
 using ShellOrientation.Views.Dialogs;
 using ShellOrientation.Views.Home;
+using System;
+using System.Threading;
 using System.Windows;
 
 namespace ShellOrientation
@@ -14,6 +16,23 @@ namespace ShellOrientation
     /// </summary>
     public partial class App
     {
+        private static Mutex _mutex = null;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "ShellOrientation";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("ShellOrientation件已开启", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                //app is already running! Exiting the application  
+                Environment.Exit(-1);
+            }
+
+            base.OnStartup(e);
+        }
         protected override Window CreateShell()
         {
             return Container.Resolve<MainWindow>();
--- ShellOrientation/Views/MainWindow.xaml.cs
+++ ShellOrientation/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.Windows;
 
 namespace ShellOrientation.Views
@@ -11,6 +12,17 @@ namespace ShellOrientation.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+
+            if (MessageBox.Show("你确定关闭软件吗？", "确认", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+            {
+
+            }
+            else
+                e.Cancel = true;
+            base.OnClosing(e);
         }
     }
 }
